1acead7a8abb205774cd4520ab04dd4c32c4bb89
codewarrior <codewarrior0330@qq.com>
2019-07-29 17:36:06 +0800

    121

---
+++ 19. Remove Nth Node From End of List(删除链表的倒数第N个节点).js
@@ -0,0 +1,49 @@
+/**
+Given a linked list, remove the n-th node from the end of list and return its head.
+Example:
+Given linked list: 1->2->3->4->5, and n = 2.
+After removing the second node from the end, the linked list becomes 1->2->3->5.
+Note:
+Given n will always be valid.
+Follow up:
+Could you do this in one pass?
+给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+示例：
+给定一个链表: 1->2->3->4->5, 和 n = 2.
+当删除了倒数第二个节点后，链表变为 1->2->3->5.
+说明：
+给定的 n 保证是有效的。
+进阶：
+你能尝试使用一趟扫描实现吗？
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+let removeNthFromEnd = function(head, n) {
+    let next = head.next, count = 1;
+    while(next !== null){
+        count++;
+        next = next.next;
+    }
+    if(count < n || n <= 0){return head}
+    if(count === 1 && n === 1){return null}
+    let tmp = head;
+    for(let i = 1; i < count - n; i++){
+        tmp = tmp.next;
+    }
+    if(count === n){
+        tmp.next = null;
+    }else{
+        tmp.next = tmp.next.next;
+    }
+    return head;
+};
