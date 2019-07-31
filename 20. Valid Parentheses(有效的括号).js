7f0149a6268e81615308ccc951b4e995a4aaadaf
codewarrior <codewarrior0330@qq.com>
2019-07-31 16:07:05 +0800

    sleepy

---
+++ 20. Valid Parentheses(有效的括号).js
@@ -0,0 +1,66 @@
+/** 
+ Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+Example 1:
+Input: "()"
+Output: true
+Example 2:
+Input: "()[]{}"
+Output: true
+Example 3:
+Input: "(]"
+Output: false
+Example 4:
+Input: "([)]"
+Output: false
+Example 5:
+Input: "{[]}"
+Output: true
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+有效字符串需满足：
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+注意空字符串可被认为是有效字符串。
+示例 1:
+输入: "()"
+输出: true
+示例 2:
+输入: "()[]{}"
+输出: true
+示例 3:
+输入: "(]"
+输出: false
+示例 4:
+输入: "([)]"
+输出: false
+示例 5:
+输入: "{[]}"
+输出: true
+*/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+let characters = {
+    '(':")",
+    '{':"}",
+    '[':"]"
+
+}; 
+let isValid = function(s) {
+    let stack = [];
+    for(let str of s){
+        if(str in characters){
+            stack.push(str);
+        }else{
+            if(characters[stack.pop()] !== str){
+                return false;
+            }
+        }
+    }
+    return stack.length === 0 ? true : false;
+};
