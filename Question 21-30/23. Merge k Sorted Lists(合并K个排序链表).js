ec2b4e917a6bac8d9c83bbf6149f2cfc2f69a2c4
codewarrior <codewarrior0330@qq.com>
2019-08-07 16:19:41 +0800

    123456

---
+++ Question 21-30/23. Merge k Sorted Lists(合并K个排序链表).js
@@ -0,0 +1,34 @@
+/** 
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+Example:
+ Input:
+ [
+   1->4->5,
+   1->3->4,
+   2->6
+ ]
+ Output: 1->1->2->3->4->4->5->6
+ 合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
+ 示例:
+ 输入:
+ [
+   1->4->5,
+   1->3->4,
+   2->6
+ ]
+ 输出: 1->1->2->3->4->4->5->6
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    
+};
