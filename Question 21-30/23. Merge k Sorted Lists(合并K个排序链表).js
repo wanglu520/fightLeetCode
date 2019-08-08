7455bec9cd1e5a4ee976ebfcfb1ff0415c6978dd
codewarrior <codewarrior0330@qq.com>
2019-08-08 17:25:27 +0800

    12345678

--- Question 21-30/23. Merge k Sorted Lists(合并K个排序链表).js
+++ Question 21-30/23. Merge k Sorted Lists(合并K个排序链表).js
@@ -29,6 +29,32 @@ Example:
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var mergeKLists = function(lists) {
-    
+let mergeKLists = function(lists) {
+    if(lists.length === 0){
+        return null;
+    }
+    if(lists.length === 1){
+        return lists[0];
+    }
+    let resList = lists[0];
+    tmpLists = lists.slice(1);
+    for(let list of tmpLists){
+        resList = mergeTwoLists(resList, list);
+    }
+    return resList;
 };
+let mergeTwoLists = (l1, l2)=>{
+    if(l1 === null){
+        return l2;
+    }
+    if(l2 === null){
+        return l1;
+    }
+    if(l1.val > l2.val){
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2
+    }else{
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    }
+}
