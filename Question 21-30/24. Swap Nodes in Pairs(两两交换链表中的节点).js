a9ca118a41bf1f4ad28f522787867d838c592bd9
codewarrior <codewarrior0330@qq.com>
2019-08-16 11:25:25 +0800

    recursion

---
+++ Question 21-30/24. Swap Nodes in Pairs(两两交换链表中的节点).js
@@ -0,0 +1,30 @@
+/** 
+Given a linked list, swap every two adjacent nodes and return its head.
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+Example:
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+示例:
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+let swapPairs = function(head) {
+    if(head === null || head.next === null){
+        return head;
+    }
+    let next = head.next;
+    head.next = swapPairs(next.next);
+    next.next = head;
+    return next;
+};
