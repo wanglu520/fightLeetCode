17712e63ef22e227f6b9f2b6b98cd9419958014c
codewarrior <codewarrior0330@qq.com>
2020-03-31 16:42:17 +0800

    1

---
+++ DailyQuestion/java/912. Sort an Array.java
@@ -0,0 +1,14 @@
+package DailyQuestion.java;
+import java.util.Arrays;
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+
+    // public int[] sortArraya(int[] nums){
+
+    // }
+
+}
