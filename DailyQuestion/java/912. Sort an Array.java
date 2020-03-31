31536fbe5860dd8abf6961d99d578f0b69415115
codewarrior <codewarrior0330@qq.com>
2020-03-31 16:52:49 +0800

    12

--- DailyQuestion/java/912. Sort an Array.java
+++ DailyQuestion/java/912. Sort an Array.java
@@ -7,8 +7,29 @@ class Solution {
         return nums;
     }
 
-    // public int[] sortArraya(int[] nums){
+    public int[] sortArraya(int[] nums){
+        if(nums.length < 2){return nums;}
+        int mid = nums.length / 2;
+        int[] left = Arrays.copyOfRange(nums, 0, mid);
+        int[] right = Arrays.copyOfRange(nums, mid, nums.length);
+        return helper(sortArraya(left), sortArraya(right));
+    }
 
-    // }
+    private int[] helper(int[] left, int[] right){
+        int[] result = new int[left.length+right.length];
+        int l = 0, r = 0;
+        for(int i = 0; i < result.length; i++){
+            if(l >= left.length){
+                result[i] = right[r++];
+            }else if(r >= right.length){
+                result[i] = left[l++];
+            }else if (left[l] > right[r]){
+                result[i] = right[r++];
+            }else{
+                result[i] = left[l++];
+            }
+        }
+        return result;
+    }
 
 }
