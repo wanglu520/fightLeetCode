0d34ce78ff6b7340e80a51f526853dc9b5233ad3
codewarrior <codewarrior0330@qq.com>
2020-04-14 16:45:08 +0800

    1

--- Question/Question 21-30/27. Remove Element.js
+++ Question/Question 21-30/27. Remove Element.js
@@ -4,5 +4,12 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-
+    let len  = nums.length;
+    let index=0;
+    for(let i=0;i<len;i++){
+        if(nums[i] != val){
+            nums[index++] = nums[i];
+        }
+    }
+    return len;
 };
--- Question/Question 21-30/27.Remove Element.java
+++ Question/Question 21-30/27.Remove Element.java
@@ -11,4 +11,14 @@ class Solution {
         }
         return len;
     }
+    public int removeElement1(int[] nums, int val) {
+        int len = nums.length;
+        int index=0;
+        for(int i=0; i<len; i++){
+            if(nums[i] != val){
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
 }
