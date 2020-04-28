7af0f5c87be0812e1dc1ccd14ce25eda7320dd46
codewarrior <codewarrior0330@qq.com>
2020-04-28 11:40:52 +0800

    3

--- DailyQuestion/java/面试题56 - I. 数组中数字出现的次数.java
+++ DailyQuestion/java/面试题56 - I. 数组中数字出现的次数.java
@@ -25,4 +25,29 @@ class Solution {
         }
         return res;
     }
+    /**
+     * 1.先按位抑或 得没有重复两个数的抑或值
+     * 2.找到这两个数二进制相同位置不同值的位置
+     * 3.通过这个位置把这两个数分道不同的组
+     * */
+    public int[] singleNumbers1(int[] nums) {
+        int orNum = 0;
+        for(int num : nums){
+            orNum ^= num;
+        }
+        int diff = 1;
+        while ((diff & orNum) == 0){
+            diff <<= 1;
+        }
+        int a = 0;
+        int b = 0;
+        for(int num : nums){
+            if((diff & num)==0){
+                a ^= num;
+            }else {
+                b ^= num;
+            }
+        }
+        return new int[]{a,b};
+    }
 }
