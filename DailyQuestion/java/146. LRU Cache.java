0480f23553b3e813a770db704bba36b2edcd1676
codewarrior <codewarrior0330@qq.com>
2020-05-25 15:07:56 +0800

    2

--- DailyQuestion/java/146. LRU Cache.java
+++ DailyQuestion/java/146. LRU Cache.java
@@ -50,7 +50,81 @@ class LRUCache {
         }
     }
 }
+//哈希+双链表
+class LRUCache1 {
+    class DLinkedNode{
+        int key;
+        int value;
+        DLinkedNode next;
+        DLinkedNode pre;
+        public DLinkedNode(){}
+        public DLinkedNode(int key, int value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+    DLinkedNode head;
+    DLinkedNode tail;
+    Map<Integer, DLinkedNode> cache = new HashMap<>();
+    int size;
+    int capacity;
+    public LRUCache1(int capacity) {
+        this.capacity = capacity;
+        size = 0;
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head.next = tail;
+        tail.pre = head;
+    }
 
+    public int get(int key){
+        DLinkedNode temp = cache.get(key);
+        if(temp == null){
+            return -1;
+        }
+        moveTohead(temp);
+        return temp.value;
+    }
+
+    public void put(int key, int value){
+        DLinkedNode temp = cache.get(key);
+        if(temp == null){
+            DLinkedNode newNode = new DLinkedNode(key, value);
+            cache.put(key, newNode);
+            addToHead(newNode);
+            size++;
+            if(size > capacity){
+                DLinkedNode res = removeTail();
+                cache.remove(res.key);
+                size--;
+            }
+        }else {
+            temp.value = value;
+            moveTohead(temp);
+        }
+
+    }
+
+    private void moveTohead(DLinkedNode node){
+        removeNode(node);
+        addToHead(node);
+    }
+    private void addToHead(DLinkedNode node){
+        node.pre = head;
+        node.next = head.next;
+        head.next.pre = node;
+        head.next = node;
+    }
+    private void removeNode(DLinkedNode node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+    }
+    private DLinkedNode removeTail(){
+        DLinkedNode res = tail.pre;
+        removeNode(res);
+        return  res;
+    }
+}
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
