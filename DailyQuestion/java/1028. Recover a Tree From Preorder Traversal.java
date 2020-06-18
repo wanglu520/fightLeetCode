98eedb245bf9efb6962450f64e19ed57dfd8ab25
codewarrior <codewarrior0330@qq.com>
2020-06-18 15:29:36 +0800

    1

---
+++ DailyQuestion/java/1028. Recover a Tree From Preorder Traversal.java
@@ -0,0 +1,52 @@
+package DailyQuestion.java;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+import Util.TreeNode;
+
+//抄作业
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode recoverFromPreorder(String S) {
+        Deque<TreeNode> path = new LinkedList<TreeNode>();
+        int pos = 0;
+        while (pos < S.length()) {
+            int level = 0;
+            while (S.charAt(pos) == '-') {
+                ++level;
+                ++pos;
+            }
+            int value = 0;
+            while (pos < S.length() && Character.isDigit(S.charAt(pos))) {
+                value = value * 10 + (S.charAt(pos) - '0');
+                ++pos;
+            }
+            TreeNode node = new TreeNode(value);
+            if (level == path.size()) {
+                if (!path.isEmpty()) {
+                    path.peek().left = node;
+                }
+            }
+            else {
+                while (level != path.size()) {
+                    path.pop();
+                }
+                path.peek().right = node;
+            }
+            path.push(node);
+        }
+        while (path.size() > 1) {
+            path.pop();
+        }
+        return path.peek();
+    }
+}
