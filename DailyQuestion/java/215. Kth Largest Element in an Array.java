a2d25aa1d2e2dfb100cc4e99c3b2f5d96784c1b6
codewarrior <codewarrior0330@qq.com>
2020-06-29 11:44:45 +0800

    1

---
+++ DailyQuestion/java/215. Kth Largest Element in an Array.java
@@ -0,0 +1,10 @@
+package DailyQuestion.java;
+
+import java.util.Arrays;
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length -k];
+    }
+}
