6d3ebd4ab4dcc732cbcb81a2abb123fc3339979e
codewarrior <codewarrior0330@qq.com>
2020-06-30 16:07:38 +0800

    1

---
+++ DailyQuestion/java/剑指 Offer 09. 用两个栈实现队列.java
@@ -0,0 +1,37 @@
+package DailyQuestion.java;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+class CQueue {
+    Deque<Integer> stack1;
+    Deque<Integer> stack2;
+    public CQueue() {
+        stack1 = new LinkedList<>();
+        stack2 = new LinkedList<>();
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        if(stack2.isEmpty()){
+            while (!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        if(stack2.isEmpty()){
+            return -1;
+        }
+        return stack2.pop();
+    }
+}
+
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
