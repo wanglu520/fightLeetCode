d18b2152689b2829ab8bab5a47d74097b4e132f5
codewarrior <codewarrior0330@qq.com>
2020-07-20 16:30:03 +0800

    2

--- DailyQuestion/java/July/167. Two Sum II - Input array is sorted.java
+++ DailyQuestion/java/July/167. Two Sum II - Input array is sorted.java
@@ -1,7 +1,8 @@
 package DailyQuestion.java.July;
 
-//暴力解
+
 class Solution {
+    //暴力解
     public int[] twoSum(int[] numbers, int target) {
         int len = numbers.length;
         for(int i=0; i<len; i++){
@@ -15,5 +16,24 @@ class Solution {
             }
         }
         return new int[0];
+    }
+    //二分查找target - numbers[i]的值
+    public int[] twoSum1(int[] numbers, int target) {
+        int len = numbers.length;
+        for(int i=0; i<len; i++){
+            int start = i+1, end = len-1;
+            while (start<=end){
+                int mid = (end - start)/2 + start;
+                int temp = target - numbers[i];
+                if(numbers[mid] == temp){
+                    return new int[]{i+1, mid+1};
+                }else if(numbers[mid] > temp){
+                    end = mid -1;
+                }else {
+                    start = mid+1;
+                }
+            }
+        }
+        return new int[]{-1,-1};
     }
 }
