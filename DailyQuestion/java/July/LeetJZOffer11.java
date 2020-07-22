8fc7a072760eca82a134fb9fae01ed64dc4ca6cc
codewarrior <codewarrior0330@qq.com>
2020-07-22 11:08:10 +0800

    二分查找

--- DailyQuestion/java/July/LeetJZOffer11.java
+++ DailyQuestion/java/July/LeetJZOffer11.java
@@ -15,4 +15,21 @@ class Solution {
         }
         return res;
     }
+    //二分查找
+    public int minArray1(int[] numbers) {
+        int start = 0;
+        int end = numbers.length-1;
+        while (start <= end){
+            int mid = (end - start)/2 + start;
+            //如果最右边大于中间的数，说明最小的在左边区域
+            if(numbers[mid] < numbers[end]){
+                end = mid;
+            }else if(numbers[mid] > numbers[end]){//中间大于最右边，说明旋转过。最小值在右边区域
+                start = mid+1;
+            }else {//如果相同，区分不了最小区域在哪边，最右边为无用值，最右边向左移动一位；
+                end--;
+            }
+        }
+        return numbers[start];
+    }
 }
