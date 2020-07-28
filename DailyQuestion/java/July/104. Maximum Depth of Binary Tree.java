ca1c7cefa78daf1e4427f76405709c7330b3d5f5
codewarrior <codewarrior0330@qq.com>
2020-07-28 09:27:05 +0800

    1

---
+++ DailyQuestion/java/July/104. Maximum Depth of Binary Tree.java
@@ -0,0 +1,26 @@
+package DailyQuestion.java.July;
+
+import Util.TreeNode;
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    //递归左右树节点
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int len = 1;
+        return Math.max(dp(root.right, len),dp(root.left, len));
+    }
+    public int dp(TreeNode treeNode, int depth){
+        //节点为空返回上一个节点深度
+        if(treeNode == null){
+            return depth;
+        }
+        depth++;
+        return Math.max(dp(treeNode.left, depth), dp(treeNode.right, depth));
+    }
+}
