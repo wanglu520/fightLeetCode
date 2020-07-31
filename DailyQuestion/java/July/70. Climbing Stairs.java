c51a8e2b279598c23219ccaeb1dfd264ed891a57
codewarrior <codewarrior0330@qq.com>
2020-07-31 10:52:08 +0800

    动态规划

--- DailyQuestion/java/July/70. Climbing Stairs.java
+++ DailyQuestion/java/July/70. Climbing Stairs.java
@@ -4,6 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
+
+    //动态规划
+    //f(x) = f(x-1) + f(x-2)
+    public int climbStairs1(int n){
+        if(n <= 0){
+            return 0;
+        }
+        int pre = 0, douPre=0, cur = 1;
+        for(int i=1; i<=n; i++){
+            douPre = pre;
+            pre = cur;
+            cur = pre + douPre;
+        }
+        return cur;
+    }
     //回溯+剪枝（去掉大于2的）
     //超时。。。
     public int climbStairs(int n) {
