067455ec1fb90bdb468b7aa8f6b7145638d3e966
codewarrior <codewarrior0330@qq.com>
2020-08-01 20:50:57 +0800

    1

---
+++ DailyQuestion/java/August/632. Smallest Range Covering Elements from K Lists.java
@@ -0,0 +1,41 @@
+package DailyQuestion.java.August;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] smallestRange(List<List<Integer>> nums) {
+        int rangeLeft = 0, rangeRight = Integer.MAX_VALUE;
+        int minRange = rangeRight - rangeLeft;
+        int max = Integer.MIN_VALUE;
+        int size = nums.size();
+        int[] next = new int[size];
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            public int compare(Integer index1, Integer index2) {
+                return nums.get(index1).get(next[index1]) - nums.get(index2).get(next[index2]);
+            }
+        });
+        for (int i = 0; i < size; i++) {
+            priorityQueue.offer(i);
+            max = Math.max(max, nums.get(i).get(0));
+        }
+        while (true) {
+            int minIndex = priorityQueue.poll();
+            int curRange = max - nums.get(minIndex).get(next[minIndex]);
+            if (curRange < minRange) {
+                minRange = curRange;
+                rangeLeft = nums.get(minIndex).get(next[minIndex]);
+                rangeRight = max;
+            }
+            next[minIndex]++;
+            if (next[minIndex] == nums.get(minIndex).size()) {
+                break;
+            }
+            priorityQueue.offer(minIndex);
+            max = Math.max(max, nums.get(minIndex).get(next[minIndex]));
+        }
+        return new int[]{rangeLeft, rangeRight};
+    }
+}
+
