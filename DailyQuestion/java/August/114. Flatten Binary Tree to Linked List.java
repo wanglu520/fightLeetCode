85a40cd8086daa507bca88877687cb8dbb8400d6
codewarrior <codewarrior0330@qq.com>
2020-08-03 10:22:25 +0800

    1

--- DailyQuestion/java/August/114. Flatten Binary Tree to Linked List.java
+++ DailyQuestion/java/August/114. Flatten Binary Tree to Linked List.java
@@ -29,8 +29,3 @@ class Solution {
         }
     }
 }
-
-作者：LeetCode-Solution
-链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/solution/er-cha-shu-zhan-kai-wei-lian-biao-by-leetcode-solu/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
---
+++ DailyQuestion/java/August/415. Add Strings.java
@@ -0,0 +1,23 @@
+class Solution {
+    //字符串从尾部开始遍历，一一相加
+    public String addStrings(String num1, String num2) {
+        StringBuilder res = new StringBuilder();
+        int len1 = num1.length()-1;
+        int len2 = num2.length()-1;
+        int temp = 0;
+        while (len1 >= 0 || len2 >= 0){
+            //转换成int类型
+            int val1 = len1 >= 0 ? num1.charAt(len1--) - '0' : 0;
+            int val2 = len2 >= 0 ? num2.charAt(len2--) - '0' : 0;
+            int tempSum = val1 + val2 + temp;
+            //大于10情况
+            res.append(tempSum % 10);
+            temp = tempSum / 10;
+        }
+        //遍历结束temp不为0，向前进一位
+        if(temp > 0){
+            res.append(temp);
+        }
+        return res.reverse().toString();
+    }
+}
