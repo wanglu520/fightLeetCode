88646ae2cf199d77c409805f07715cfffad50fbc
codewarrior <codewarrior0330@qq.com>
2021-03-25 22:17:08 +0800

    Leet82

---
+++ DailyQuestion/java/March/82. Remove Duplicates from Sorted List II.java
@@ -0,0 +1,32 @@
+package DailyQuestion.java.March;
+
+import Util.ListNode;
+
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+ * ListNode next) { this.val = val; this.next = next; } }
+ */
+class Solution {
+  public ListNode deleteDuplicates(ListNode head) {
+      if (head == null) {
+          return head;
+      }
+
+      ListNode temp = new ListNode(-1, head);
+
+      ListNode cur = temp;
+      while (cur.next != null && cur.next.next != null) {
+          if (cur.next.val == cur.next.next.val) {
+              int x = cur.next.val;
+              while (cur.next != null && cur.next.val == x) {//相同往后移
+                  cur.next = cur.next.next;
+              }
+          } else {
+              cur = cur.next;
+          }
+      }
+
+      return temp.next;
+  }
+}
--- Util/ListNode.java
+++ Util/ListNode.java
@@ -1,7 +1,9 @@
 package Util;
 
 public class ListNode {
-     int val;
+     public int val;
      public ListNode next;
-     ListNode(int x) { val = x; }
-}
+     public ListNode() {}
+     public ListNode(int val) { this.val = val; }
+     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
