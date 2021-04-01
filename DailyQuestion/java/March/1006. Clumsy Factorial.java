6e8cfcf0f8b6a8ad4bca9849778b2da23e90f6f1
codewarrior <codewarrior0330@qq.com>
2021-04-01 10:42:14 +0800

    Leet1006

--- DailyQuestion/java/March/1006. Clumsy Factorial.java
+++ DailyQuestion/java/March/1006. Clumsy Factorial.java
@@ -23,4 +23,24 @@ class Solution {
       }
       return N > 4 ? res-temp : res+temp;
   }
+
+  public int clumsy1(int N) {
+    if (N == 1) {
+        return 1;
+    } else if (N == 2) {
+        return 2;
+    } else if (N == 3) {
+        return 6;
+    } else if (N == 4) {
+        return 7;
+    }
+
+    if (N % 4 == 0) {
+        return N + 1;
+    } else if (N % 4 <= 2) {
+        return N + 2;
+    } else {
+        return N - 1;
+    }
+  }
 }
