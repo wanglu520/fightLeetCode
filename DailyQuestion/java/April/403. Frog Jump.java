8df68e8663a763f173602eb3ed0bd47b38713cb0
codewarrior <codewarrior0330@qq.com>
2021-04-29 22:41:10 +0800

    Leet403

---
+++ DailyQuestion/java/April/403. Frog Jump.java
@@ -0,0 +1,28 @@
+class Solution {
+  private Boolean[][] rec;
+
+  public boolean canCross(int[] stones) {
+      int n = stones.length;
+      rec = new Boolean[n][n];
+      return dfs(stones, 0, 0);
+  }
+
+  private boolean dfs(int[] stones, int i, int lastDis) {
+      if (i == stones.length - 1) {
+          return true;
+      }
+      if (rec[i][lastDis] != null) {
+          return rec[i][lastDis];
+      }
+
+      for (int curDis = lastDis - 1; curDis <= lastDis + 1; curDis++) {
+          if (curDis > 0) {
+              int j = Arrays.binarySearch(stones, i + 1, stones.length, curDis + stones[i]);
+              if (j >= 0 && dfs(stones, j, curDis)) {
+                  return rec[i][lastDis] = true;
+              }
+          }
+      }
+      return rec[i][lastDis] = false;
+  }
+}
