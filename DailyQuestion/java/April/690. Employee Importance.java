3660b9d34a5d180a7aa8fe77c9ef92393e847468
codewarrior <codewarrior0330@qq.com>
2021-05-01 09:48:22 +0800

    DFS

--- DailyQuestion/java/April/690. Employee Importance.java
+++ DailyQuestion/java/April/690. Employee Importance.java
@@ -17,6 +17,7 @@ class Solution {
       for(Employee em : employees){
           map.put(em.id, em);
       }
+      //通过队列
       while (!queue.isEmpty()){
           int emId = queue.poll();
           Employee tempEm = map.get(emId);
@@ -26,5 +27,23 @@ class Solution {
           }
       }
       return res;
+  }
+  Map<Integer, Employee> map = new HashMap<>();
+  //dfs
+  public int getImportance1(List<Employee> employees, int id) {
+      Map<Integer, Employee> map = new HashMap<>();
+      for(Employee em : employees){
+          map.put(em.id, em);
+      }
+      return dfs(id);
+  }
+  public int dfs(int id){
+      Employee em = map.get(id);
+      int total = em.importance;
+      List<Integer> subordinates = em.subordinates;
+      for(int subId : subordinates){
+          total += dfs(subId);
+      }
+      return total;
   }
 }
