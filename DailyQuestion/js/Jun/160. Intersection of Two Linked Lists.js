d99cd7da06d19bdc06e3c68a48f10f498feedaf9
codewarrior <codewarrior0330@qq.com>
2021-06-04 21:37:23 +0800

    Leet160

---
+++ DailyQuestion/js/Jun/160. Intersection of Two Linked Lists.js
@@ -0,0 +1,12 @@
+//走到尽头见不到你，于是走过你来时的路，等到相遇时才发现，你也走过我来时的路❤
+var getIntersectionNode = function(headA, headB) {
+  if (headA === null || headB === null) {
+      return null;
+  }
+  let pA = headA, pB = headB;
+  while (pA !== pB) {
+      pA = pA === null ? headB : pA.next;
+      pB = pB === null ? headA : pB.next;
+  }
+  return pA;
+};
