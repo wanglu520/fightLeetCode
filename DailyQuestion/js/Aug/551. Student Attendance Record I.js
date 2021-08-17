51ddaac18e0c4462300f02caee350a69ba1e8255
codewarrior <codewarrior0330@qq.com>
2021-08-17 20:31:58 +0800

    checkRecord

---
+++ DailyQuestion/js/Aug/551. Student Attendance Record I.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var checkRecord = function(s) {
+  const len = s.length;
+  let countA=0, countL=0;
+  for(let i=0; i<len; i++){
+      if(s[i] === "A"){
+              countA++;
+      }
+      if(s[i] === "L"){
+          countL++;
+      }else {
+          countL = 0;
+      }
+      if(countA >= 2 || countL >= 3){
+          return false;
+      }
+  }
+  return true;
+};
