49e006c4aa5a7db16f5713d5cc50e6ab610e7fc2
codewarrior <codewarrior0330@qq.com>
2021-08-29 22:00:08 +0800

    sumOddLengthSubarrays1

--- DailyQuestion/js/Aug/1588. Sum of All Odd Length Subarrays.js
+++ DailyQuestion/js/Aug/1588. Sum of All Odd Length Subarrays.js
@@ -2,6 +2,7 @@
  * @param {number[]} arr
  * @return {number}
  */
+  //暴力
  var sumOddLengthSubarrays = function(arr) {
   const len = arr.length;
   let sum=0, left;
@@ -9,6 +10,24 @@
       left = 0;
       for(let j=i+1; j<=len; j++){
           sum += add(arr.slice(left++, j))
+      }
+  }
+  return sum;
+};
+//前缀和
+var sumOddLengthSubarrays = function(arr) {
+  const len = arr.length;
+  const preSums  = [0];
+  //计算前缀和
+  for(let i=0; i<len; i++){
+    preSums[i+1] = preSums[i] + arr[i];
+  }
+  let sum=preSums[len], start;
+  for(let i=2; i<len; i+=2){
+      start = 0;
+      for(let j=i+1; j<=len; j++){
+          //通过前缀和计算数组和
+          sum += preSums[j] - preSums[start++];
       }
   }
   return sum;
