99290b99439b5da1417e1c00d9e5839ff287849b
codewarrior <codewarrior0330@qq.com>
2021-08-31 20:41:36 +0800

    corpFlightBookings1

--- DailyQuestion/js/Aug/1109. Corporate Flight Bookings.js
+++ DailyQuestion/js/Aug/1109. Corporate Flight Bookings.js
@@ -14,3 +14,20 @@
   }
   return answer;
 };
+//差分 [1,2,10],[2,3,20]
+//10 0 -10
+//   20 0 
+//answer=[10, 10+20=30, 10+(-10)+20=20] 
+var corpFlightBookings = function(bookings, n) {
+  const answer = new Array(n).fill(0);
+  for(let booking of bookings){
+    answer[booking[0] - 1] += booking[2];
+    if(booking[1] < n){
+      answer[booking[1]] -= booking[2]
+    }
+  }
+  for(let i=1; i<n; i++){
+    answer[i] += answer[i-1];
+  }
+  return answer;
+}
