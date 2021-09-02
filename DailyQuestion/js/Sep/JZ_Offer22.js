733a58ac733e7811f2b0f749e259d22f25159d97
codewarrior <codewarrior0330@qq.com>
2021-09-02 09:44:48 +0800

    getKthFromEnd1

--- DailyQuestion/js/Sep/JZ_Offer22.js
+++ DailyQuestion/js/Sep/JZ_Offer22.js
@@ -21,3 +21,15 @@
    }
    return slow;
 };
+//遍历两次，第一次确认链表长度，第二次循环len-k次
+var getKthFromEnd = function(head, k) {
+  let node = head, len = 0;
+  while(node){
+    [node, len] = [node.next, len-1];
+  }
+  node = head;
+  for(let i=0; i<len - k; i++){
+    node = node.next;
+  }
+  return node;
+}
