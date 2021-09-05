958c5f1a47a9bcadefab5fa26e6925312c518e0a
codewarrior <codewarrior0330@qq.com>
2021-09-05 21:17:29 +0800

    rand10

--- DailyQuestion/js/Sep/470. Implement Rand10() Using Rand7().js
+++ DailyQuestion/js/Sep/470. Implement Rand10() Using Rand7().js
@@ -7,7 +7,7 @@
   return (rand7()+rand7()+rand7()+rand7()+rand7()+rand7()+rand7()+rand7()+rand7()+rand7())%10+1; 
 };
 //拒绝采样
-//逆模因 first为[1,6]奇偶概率为1/2
+//for逆模因 first为[1,6]奇偶概率为1/2,
 var rand10 = function() {
   let first, second;
   do{
@@ -18,3 +18,12 @@ var rand10 = function() {
   }while(second > 5);
   return first % 2 === 1 ? second : 5 + second;
 };
+//for jerry (rand7() - 1) * 7 => {0, 7, 14, 21, 28, 35, 42}  乘7可以保证均等概率 不会出现类似2+3和3+2的情况
+//(rand7() - 1) * 7 + rand7()=>[1,49] 取前1-40可以保证随机1-10均等概率
+var rand10 = function() {
+  let num;
+  do{
+    num = (rand7() - 1) * 7 + rand7();
+  }while(num > 40);
+  return 1 + num % 10;
+}
