bbf119e29c4d0a796a33ed300dc82aa0550d04c3
codewarrior <codewarrior0330@qq.com>
2021-09-05 16:58:17 +0800

    rand10

--- DailyQuestion/js/Sep/470. Implement Rand10() Using Rand7().js
+++ DailyQuestion/js/Sep/470. Implement Rand10() Using Rand7().js
@@ -6,3 +6,15 @@
  var rand10 = function() {
   return (rand7()+rand7()+rand7()+rand7()+rand7()+rand7()+rand7()+rand7()+rand7()+rand7())%10+1; 
 };
+//拒绝采样
+//逆模因 first为[1,6]奇偶概率为1/2
+var rand10 = function() {
+  let first, second;
+  do{
+    first = rand7();
+  }while(first > 6);
+  do{
+    second = rand7();
+  }while(second > 5);
+  return first % 2 === 1 ? second : 5 + second;
+};
