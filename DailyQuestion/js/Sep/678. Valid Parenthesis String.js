ab9bb4d333fb77339554c4f9096c51f7dfd07588
codewarrior <codewarrior0330@qq.com>
2021-09-12 17:28:10 +0800

    checkValidString

--- DailyQuestion/js/Sep/678. Valid Parenthesis String.js
+++ DailyQuestion/js/Sep/678. Valid Parenthesis String.js
@@ -44,3 +44,34 @@
   }
   return true;
 };
+//栈，优化上面方法
+var checkValidString = function(s) {
+  let len = -1;
+  //arrLeftIndex保存（索引，arrStarIndex保存*索引
+  const arrLeftIndex=[], arrStarIndex=[];
+  for(let str of s){
+      if(str === "("){
+          arrLeftIndex.push(++len);
+      }else if(str === "*"){
+          arrStarIndex.push(++len);
+      }else if(str === ")"){
+          //有（先匹配
+          if(arrLeftIndex.length > 0){
+              arrLeftIndex.pop();
+          }else if(arrStarIndex.length > 0){//用*匹配
+              arrStarIndex.pop();
+          }else{//（和*都么有就不是有效的
+              return false;
+          }
+      }
+  }
+  while(arrLeftIndex.length && arrStarIndex.length){
+    const leftInde = arrLeftIndex.pop();
+    const starIndex = arrStarIndex.pop();
+    //(的索引比*的大，说明（右侧没有与之匹配的
+    if(leftInde > starIndex){
+      return false;
+    }
+  }
+  return arrLeftIndex.length === 0;
+};
