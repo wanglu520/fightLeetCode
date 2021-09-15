5a210d7185043ca1e45f5d2b9c508760627e4dfc
codewarrior <codewarrior0330@qq.com>
2021-09-15 09:56:39 +0800

    findPeakElement

--- DailyQuestion/js/Sep/162. Find Peak Element.js
+++ DailyQuestion/js/Sep/162. Find Peak Element.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 //nums[i] != nums[i+1],如果i不满足，那么满足的一定在上坡趋势的方向（nums[i]>nums[i+1]往左，num[i]<num[i+1]往右）
+//上升趋势是有序的，可以用二分搜索
  var findPeakElement = function(nums) {
   let left = 0,right = nums.length - 1;
   while(left < right){
