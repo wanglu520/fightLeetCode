eee2d54e3b16a356be6d0b85d8e29ed0da8bc2fc
codewarrior <codewarrior0330@qq.com>
2021-09-22 10:38:34 +0800

    splitListToParts

--- DailyQuestion/js/Sep/725. Split Linked List in Parts.js
+++ DailyQuestion/js/Sep/725. Split Linked List in Parts.js
@@ -22,21 +22,44 @@
   let total = len / k | 0, sur = len % k;
   temp = head;
   while(temp !== null){
+        //前len % k个链表长度+1
       let count = total + (sur-- > 0 ? 1 : 0);
       const curr = temp;
       while(count > 1 && temp !== null){
           temp = temp.next;
           count--;
       }
-      if(temp !== null){
-          const next = temp.next;
-          temp.next = null;
-          temp = next;
-      }
+      const next = temp.next;
+      temp.next = null;
+      temp = next;
       res.push(curr);
   }
   for(let i = k-len; i>0; i--){
       res.push(null);
   }
   return res;
+};
+
+var splitListToParts = function(head, k) {
+  let n = 0;
+  let temp = head;
+  while (temp != null) {
+      n++;
+      temp = temp.next;
+  }
+  let quotient = Math.floor(n / k), remainder = n % k;
+
+  const parts = new Array(k).fill(null);
+  let curr = head;
+  for (let i = 0; i < k && curr != null; i++) {
+      parts[i] = curr;
+      let partSize = quotient + (i < remainder ? 1 : 0);
+      for (let j = 1; j < partSize; j++) {
+          curr = curr.next;
+      }
+      const next = curr.next;
+      curr.next = null;
+      curr = next;
+  }
+  return parts;
 };
