115198ee2db715ae335d58209eb4a249369d789e
codewarrior <codewarrior0330@qq.com>
2021-09-30 21:01:02 +0800

    computeArea

---
+++ DailyQuestion/js/Sep/223. Rectangle Area.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} ax1
+ * @param {number} ay1
+ * @param {number} ax2
+ * @param {number} ay2
+ * @param {number} bx1
+ * @param {number} by1
+ * @param {number} bx2
+ * @param {number} by2
+ * @return {number}
+ */
+ var computeArea = function(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2) {
+  const areaA = (ax2 - ax1) * (ay2 - ay1);
+  const areaB = (bx2 - bx1) * (by2 - by1);
+  const overlapy = Math.min(ay2, by2) - Math.max(ay1, by1);
+  const overlapx = Math.min(ax2, bx2) - Math.max(ax1, bx1);
+  const overlapArea = Math.max(overlapx, 0) * Math.max(overlapy, 0);
+  return areaA + areaB - overlapArea;
+};
