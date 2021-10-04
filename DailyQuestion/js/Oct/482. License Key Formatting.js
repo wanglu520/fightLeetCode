128997f23b196c718aaf712acb482347433ba0f6
codewarrior <codewarrior0330@qq.com>
2021-10-04 23:35:08 +0800

    licenseKeyFormatting

--- DailyQuestion/js/Oct/482. License Key Formatting.js
+++ DailyQuestion/js/Oct/482. License Key Formatting.js
@@ -16,10 +16,12 @@
           temp = temp.toUpperCase();
       }
       res.push(temp);
+      //k的倍数加分隔符
       if(index % k === 0){
           res.push("-");
       }
   }
+  //处理特殊情况，最后一位是“-”
   if(res.length > 0 && res[res.length - 1] === "-"){
       res.pop();
   }
