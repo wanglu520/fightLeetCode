70a00328f02dc87a0e6a278c4f6c3ccdad499532
codewarrior <codewarrior0330@qq.com>
2021-10-10 19:24:16 +0800

    arrangeCoins

---
+++ DailyQuestion/js/Oct/441. Arranging Coins.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function(n) {
+  let left = 1, right = n;
+  while (left < right) {
+      const mid = Math.floor((right - left + 1) / 2) + left;
+      if (mid * (mid + 1) <= 2 * n) {
+          left = mid;
+      } else {
+          right = mid - 1;
+      }
+  }
+  return left;
+};
