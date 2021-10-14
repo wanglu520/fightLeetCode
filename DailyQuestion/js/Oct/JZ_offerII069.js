f527330f4aec917c7eb1166afecfa65a74b621a7
codewarrior <codewarrior0330@qq.com>
2021-10-15 00:01:56 +0800

    peakIndexInMountainArray1

--- DailyQuestion/js/Oct/JZ_offerII069.js
+++ DailyQuestion/js/Oct/JZ_offerII069.js
@@ -14,3 +14,19 @@ var peakIndexInMountainArray = function(arr) {
   }
   return ans;
 };
+
+const peakIndexInMountainArray1 = function(arr) {
+  const n = arr.length;
+  let left = 1, right = n - 2, ans = 0;
+
+  while (left <= right) {
+      const mid = Math.floor((left + right) /2 );
+      if (arr[mid] > arr[mid + 1]) {
+          ans = mid;
+          right = mid - 1;
+      } else {
+          left = mid + 1;
+      }
+  }
+  return ans;
+};
