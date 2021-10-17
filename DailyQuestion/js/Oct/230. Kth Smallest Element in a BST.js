05d095adb7b9ba8a1ec612119f3e3c597c9a62bd
codewarrior <codewarrior0330@qq.com>
2021-10-17 20:13:01 +0800

    kthSmallest

--- DailyQuestion/js/Oct/230. Kth Smallest Element in a BST.js
+++ DailyQuestion/js/Oct/230. Kth Smallest Element in a BST.js
@@ -28,3 +28,53 @@
   }
   return root.val;
 };
+
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+ var kthSmallest = function(root, k) {
+  const myBst = new MyBst(root);
+  return myBst.kthSmallest(k);
+};
+class MyBst{
+  constructor(root){
+      this.root = root;
+      this.nodeNumMap = new Map();
+      this.countNodeNum(root);
+  }
+  kthSmallest(k){
+      if(this.getNodeNum(this.root) < k){
+          return -1;
+      }
+      let node = this.root;
+      while(node !== null){
+          //左边节点必定小于node
+          const leftNum = this.getNodeNum(node.left);
+          if(leftNum < k - 1){//结果在右边
+              k -= leftNum + 1;
+              node = node.right;
+          }else if(leftNum > k - 1){//结果在左边
+              node = node.left;
+          }else{
+              break;
+          }
+      }
+      return node !== null ? node.val : -1;
+  }
+  //统计当前节点的子节点个数
+  countNodeNum(node){
+      if(node === null){
+          return 0;
+      }
+      this.nodeNumMap.set(
+          node,
+          1 + this.countNodeNum(node.left) + this.countNodeNum(node.right)
+      )
+      return this.nodeNumMap.get(node);
+  }
+  getNodeNum(node){
+      return this.nodeNumMap.get(node) || 0;
+  }
+}
