9d1082e98174fb9eeffb86d93f68b99206bca811
codewarrior <codewarrior0330@qq.com>
2021-10-23 08:57:31 +0800

    constructRectangle

---
+++ DailyQuestion/js/Oct/492. Construct the Rectangle.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} area
+ * @return {number[]}
+ */
+ var constructRectangle = function(area) {
+  let w = Math.floor(Math.sqrt(area));
+  while(area % w !== 0){
+      w--;
+  }
+  return [Math.floor(area/w), w];
+};
