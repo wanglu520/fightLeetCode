72809413d8c0884984ed1183cebe3ce10deedef4
codewarrior <codewarrior0330@qq.com>
2021-10-30 16:14:31 +0800

    singleNumber

--- DailyQuestion/js/Oct/260. Single Number III.js
+++ DailyQuestion/js/Oct/260. Single Number III.js
@@ -16,3 +16,30 @@
    }
    return res;
 };
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+//位运算
+ var singleNumber = function(nums) {
+  let xorNum = 0;
+  //获得两个不相同数字的异或值；
+  for(const num of nums){
+    xorNum ^= num;
+  }
+  //找到两个不同数字，二进制位上最低位不相同数的位置
+  let diffNum = 1;
+  while((xorNum & diffNum) === 0){
+    diffNum <<= 1;
+  }
+  //通过diffNum把数组分成两部分。每个部分最终的结果就是要找的数字
+  let num1 = 0, num2 = 0;
+  for(const tempNum of nums){
+    if(tempNum & diffNum){
+      num1 ^= tempNum;
+    }else{
+      num2 ^= tempNum;
+    }
+  }
+  return [num1, num2]
+}
