0fa4765f88c0bac3c680d17ca8e618e9102ebb5d
codewarrior <codewarrior0330@qq.com>
2021-11-06 17:19:49 +0800

    missingNumber

--- DailyQuestion/js/Nov/268. Missing Number.js
+++ DailyQuestion/js/Nov/268. Missing Number.js
@@ -12,3 +12,16 @@
   }
   return nums.length;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ //位运算 按位异或1^1=0
+ var missingNumber = function(nums) {
+  let missNum = nums.length;
+  for(let i=0; i<nums.length; i++){
+      missNum ^= nums[i] ^ i;
+  }
+  return missNum;
+};
