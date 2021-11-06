98b3ecac01ee41e586ecdfa6e5449db6d3b3aedf
codewarrior <codewarrior0330@qq.com>
2021-11-06 17:16:47 +0800

    missingNumber

---
+++ DailyQuestion/js/Nov/268. Missing Number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+//排序
+ var missingNumber = function(nums) {
+  nums.sort((a, b)=>a-b)
+  for(let i = 0; i < nums.length; i++){
+      if(nums[i] != i){
+          return i;
+      }
+  }
+  return nums.length;
+};
