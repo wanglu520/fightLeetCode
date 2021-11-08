b3f7d0bd91a67dc634a6b5d1a206916e2c18dd0a
codewarrior <codewarrior0330@qq.com>
2021-11-08 20:49:34 +0800

    getHint

--- DailyQuestion/js/Nov/299. Bulls and Cows.js
+++ DailyQuestion/js/Nov/299. Bulls and Cows.js
@@ -10,6 +10,7 @@
   for(const str of secret){
       map.set(str, (map.get(str)||0) + 1)
   }
+  //公牛数
   for(let i = 0; i<guess.length; i++){
       let count = map.get(guess[i]);
       if(secret[i] === guess[i]){
@@ -18,6 +19,7 @@
       }
       
   }
+  //母牛数
   for(let i = 0; i<guess.length; i++){
       let count = map.get(guess[i]);
       if(count && secret[i] !== guess[i]){
@@ -26,4 +28,24 @@
       } 
   }
   return `${x}A${y}B`;
+};
+var getHint = function(secret, guess) {
+  //保存位置不同，数字相同secret的数字的个数
+  let conS = new Array(10).fill(0)
+  //保存位置不同，数字相同guess的数字的个数
+  let conG = new Array(10).fill(0)
+  let x = 0
+  for(let i=0; i<guess.length; i++){
+    if(secret[i] === guess[i]){
+      x++;
+    }else{
+      conS[secret[i] - 0]++;
+      conG[guess[i] - 0]++;
+    }
+  }
+  let y = 0;
+  for(let i = 0; i<10; i++){
+    y += Math.min(conS[i], conG[i]);
+  }
+  return `${x}A${y}B`
 };
