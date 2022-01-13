9a12a313113f83c55392e06858c99dc3074cf730
codewarrior <codewarrior0330@qq.com>
2022-01-13 21:26:03 +0800

    dominantIndex

---
+++ DailyQuestion/js/JAN/2022/747. Largest Number At Least Twice of Others.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var dominantIndex = function(nums) {
+  let m1 = -1, m2 = -1;
+  let index = -1;
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > m1) {
+          m2 = m1;
+          m1 = nums[i];
+          index = i;
+      } else if (nums[i] > m2) {
+          m2 = nums[i];
+      }
+  }
+  return m1 >= m2 * 2 ? index : -1;
+};
