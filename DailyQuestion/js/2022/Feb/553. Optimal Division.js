d621b9752037b33ae933d042402bbcb5e5e010da
codewarrior <codewarrior0330@qq.com>
2022-02-27 11:25:07 +0800

    optimalDivision

---
+++ DailyQuestion/js/2022/Feb/553. Optimal Division.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ * 要得最大值，分子越大，分母越小
+ */
+ var optimalDivision = function(nums) {
+  const n = nums.length;        
+  if (n === 1) {
+      return '' + nums[0];
+  }
+  if (n === 2) {
+      return '' + nums[0] + "/" + '' + nums[1];
+  }
+  const res = [];
+  res.push(nums[0]);
+  res.push("/(");
+  res.push(nums[1]);
+  for (let i = 2; i < n; i++) {
+      res.push("/");
+      res.push(nums[i]);
+  }
+  res.push(")");
+  return res.join('');
+};
