623773a6c412d214af4c0b1d42deeabd0e923457
codewarrior <codewarrior0330@qq.com>
2022-03-28 11:12:28 +0800

    hasAlternatingBits

--- DailyQuestion/js/2022/Mar/693. Binary Number with Alternating Bits.js
+++ DailyQuestion/js/2022/Mar/693. Binary Number with Alternating Bits.js
@@ -13,3 +13,12 @@
   }
   return true;
 };
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+ var hasAlternatingBits = function(n) {
+  const a = n ^ (n >> 1);
+  return (a & (a + 1)) === 0;
+};
